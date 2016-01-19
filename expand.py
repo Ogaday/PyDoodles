1f3b02eefc3ed5bc2d64cbce800252be0b29ad98
Ogaday <Ogaday@hotmail.co.uk>
2016-01-19 02:19:18 +0000

    New code gold challenge

---
+++ expand.py
@@ -0,0 +1,54 @@
+# http://codegolf.stackexchange.com/questions/68939/expand-the-number
+ 
+def f(n):
+ if not eval(n):return "0"
+ x=n.split('.');a,b=(x+[''],x)[len(x)-1];p='+.';p=(p,'.')[a=='0'];p=(p,'')[b==''];e=enumerate;return "+".join([d+'0'*i for i,d in e(a[::-1]) if eval(d)][::-1])+p+"+.".join(['0'*i+d for i,d in e(b) if eval(d)])
+ 
+
+
+#          [d+'0'*i for i,d in enumerate(a[::-1]) if eval(d)][::-1]
+#          [d+'0'*i for i,d in zip(range(0,len(a),-1),a) if eval(d)]
+#l=len
+e=lambda s:[d+'0'*(len(s)-i-1) for i,d in enumerate(s) if eval(d)]
+def f(n):x=n.split('.');a,b=(x+[''],x)[len(x)-1];return['0',"+".join(e(a)+['.'+d[::-1]for d in e(b[::-1])][::-1])][bool(eval(n))]
+                          
+#def f(n):x=n.split('.');b=('',x[1])[len(x)-1];return['0',"+".join(e(x[0])+['.'+d[::-1]for d in e(b[::-1])][::-1])][bool(eval(n))]                        
+#def f(n):x=n.split('.');try:b=x[1]except:b='';return ['0',"+".join(e(x[0])+['.'+d[::-1] for d in e(b[::-1])][::-1])][bool(eval(n))]
+
+def f(n):
+ if not eval(n):return "0"
+ x=n.split('.');a,b=(x+[''],x)[len(x)-1];e=enumerate;return"+".join([d+'0'*i for i,d in e(a[::-1]) if eval(d)][::-1]+['.'+'0'*i+d for i,d in e(b) if eval(d)])
+
+def f(n):x=n.split('.');a,b=(x+[''],x)[len(x)-1];e=enumerate;return('0',"+".join([d+'0'*i for i,d in e(a[::-1]) if eval(d)][::-1]+['.'+'0'*i+d for i,d in e(b) if eval(d)]))[bool(eval(n))]
+
+def f(n):x=n.split('.');a,b=(x+[''],x)[len(x)-1];e=enumerate;return('0',"+".join([d+'0'*i for i,d in e(a[::-1]) if d!='0'][::-1]+['.'+'0'*i+d for i,d in e(b) if d!='0']))[eval(n)!=0]
+#                                            bool(eval(n))
+#def f(n):x=n.split('.');a,b=(x+[''],x)[len(x)-1];e=enumerate;return('0',"+".join([d+'0'*i for i,d in e(a[::-1]) if d!='0'][::-1]+['.'+'0'*i+d for i,d in e(b) if d!='0']))[n!='0']
+
+#enumerateenumerate
+#e=enumerateee;    
+
+# 164 bytes.
+def f1(n):
+ m=list(n)
+ try:p=m.index('.');m.pop(p)
+ except: p=len(m)
+ return('0','+'.join([str(eval(d)*10**(p-i-1)) for i,d in enumerate(m) if d!='0']))[eval(n)!=0]
+
+# 189 bytes
+def f1(n):
+ m,o=list(n),'0'
+ try:p=m.index('.');m.pop(p)
+ except: p=len(m)
+ return(o,'+'.join([d+o*(p-i-1) if (p-i)>0 else '0.'+o*(-1*(p-i))+d for i,d in enumerate(m) if d!=o]))[eval(n)!=0]
+ #return(o,'+'.join([d+o*u if u>0 else '0.'+o*u+d for u,d in [p-i,m[i] for i in range(len(m))] if d!=o]))[eval(n)!=0]
+#f=lambda n:'+'.join([str(eval(d)*10**(i + n.index('.'))) for i,d in enumerate(list(n).pop(n.index('.')))])
+"""
+import sys
+if __name__=="__main__" and sys.version_info[0]<3:
+    i=input().split(".")
+    I,e=i[0],enumerate
+    o=[(k+len(I[j+1::])*"0") for j,k in e(I) if k!="0"] 
+    try:J=i[1];o+=["."+l*"0"+m for l,m in e(J) if m!="0"]
+    except:''
+    print("+".join(o if o else "0"))
---
+++ test_expand.py
@@ -0,0 +1,47 @@
+import unittest
+from expand import f, f1
+
+class TestExpand(unittest.TestCase):
+
+    def test1(self):
+        #print(f("0"))
+        self.assertEqual(f("0"), "0")
+    def test2(self):
+        #print(f("6"))
+        self.assertEqual(f("6"), "6")
+    def test3(self):
+        #print(f("0.99"))
+        self.assertEqual(f("0.99"), ".9+.09")
+    def test4(self):
+        #print(f("24601"))
+        self.assertEqual(f("24601"), "20000+4000+600+1")
+    def test5(self):
+        #print(f("6.283"))
+        self.assertEqual(f("6.283"), "6+.2+.08+.003")
+    def test6(self):
+        f("9000000.0000009")
+        self.assertEqual(f("9000000.0000009"), "9000000+.0000009")
+        
+class TestExpandb(unittest.TestCase):
+
+    def test1(self):
+        #print(f("0"))
+        self.assertEqual(f1("0"), "0")
+    def test2(self):
+        #print(f("6"))
+        self.assertEqual(f1("6"), "6")
+    def test3(self):
+        #print(f("0.99"))
+        self.assertEqual(f1("0.99"), "0.9+0.09")
+    def test4(self):
+        #print(f("24601"))
+        self.assertEqual(f1("24601"), "20000+4000+600+1")
+    def test5(self):
+        #print(f("6.283"))
+        self.assertEqual(f1("6.283"), "6+0.2+0.08+0.003")
+    def test6(self):
+        f("9000000.0000009")
+        self.assertEqual(f1("9000000.0000009"), "9000000+0.0000009")
+
+if __name__ == '__main__':
+    unittest.main()

168cd3ba8996323ca4366db83b7f453bb9d28cd8
Ogaday <W.Ogaday@gmail.com>
2016-01-19 11:19:08 +0000

    Succesful golfing!

--- expand.py
+++ expand.py
@@ -21,7 +21,7 @@ def f(n):
 
 def f(n):x=n.split('.');a,b=(x+[''],x)[len(x)-1];e=enumerate;return('0',"+".join([d+'0'*i for i,d in e(a[::-1]) if eval(d)][::-1]+['.'+'0'*i+d for i,d in e(b) if eval(d)]))[bool(eval(n))]
 
-def f(n):x=n.split('.');a,b=(x+[''],x)[len(x)-1];e=enumerate;return('0',"+".join([d+'0'*i for i,d in e(a[::-1]) if d!='0'][::-1]+['.'+'0'*i+d for i,d in e(b) if d!='0']))[eval(n)!=0]
+def f(n):x=n.split('.');a,b=(x+[''],x)[len(x)-1];e=enumerate;return('0',"+".join([d+'0'*i for i,d in e(a[::-1])if d!='0'][::-1]+['.'+'0'*i+d for i,d in e(b)if d!='0']))[eval(n)!=0]
 #                                            bool(eval(n))
 #def f(n):x=n.split('.');a,b=(x+[''],x)[len(x)-1];e=enumerate;return('0',"+".join([d+'0'*i for i,d in e(a[::-1]) if d!='0'][::-1]+['.'+'0'*i+d for i,d in e(b) if d!='0']))[n!='0']
 
@@ -30,18 +30,26 @@ def f(n):x=n.split('.');a,b=(x+[''],x)[len(x)-1];e=enumerate;return('0',"+".join
 
 # 164 bytes.
 def f1(n):
- m=list(n)
+ *m,=n
  try:p=m.index('.');m.pop(p)
- except: p=len(m)
- return('0','+'.join([str(eval(d)*10**(p-i-1)) for i,d in enumerate(m) if d!='0']))[eval(n)!=0]
+ except:p=len(m)
+ #if'.'in n:p=m.index('.');m.pop(p)
+ #else:p=len(m)
+ return('0','+'.join([str(eval(d)*10**(p-i-1))for i,d in enumerate(m)if d!='0']))[eval(n)!=0] 
+
+#;m.pop(p)
+#[:p]+m[p:]
+#m,o=list(n),'0'
+#*m,=n; o='0'
 
 # 189 bytes
-def f1(n):
- m,o=list(n),'0'
+def f2(n):
+ *m,=n;o='0'
  try:p=m.index('.');m.pop(p)
- except: p=len(m)
- return(o,'+'.join([d+o*(p-i-1) if (p-i)>0 else '0.'+o*(-1*(p-i))+d for i,d in enumerate(m) if d!=o]))[eval(n)!=0]
- #return(o,'+'.join([d+o*u if u>0 else '0.'+o*u+d for u,d in [p-i,m[i] for i in range(len(m))] if d!=o]))[eval(n)!=0]
+ except:p=len(m)
+#return(o,'+'.join([d+o*(p-i-1) if (p-i)>0 else '0.'+o*(-1*(p-i))+d for i,d in enumerate(m) if d!=o]))[eval(n)!=0]
+ return(o,'+'.join([['.'+o*(-1*(p-i))+d,d+o*(p-i-1)][p-i>0]for i,d in enumerate(m)if d!=o]))[eval(n)!=0]
+#return(o,'+'.join([d+o*u if u>0 else '0.'+o*u+d for u,d in [p-i,m[i] for i in range(len(m))] if d!=o]))[eval(n)!=0]
 #f=lambda n:'+'.join([str(eval(d)*10**(i + n.index('.'))) for i,d in enumerate(list(n).pop(n.index('.')))])
 """
 import sys
@@ -52,3 +60,4 @@ if __name__=="__main__" and sys.version_info[0]<3:
     try:J=i[1];o+=["."+l*"0"+m for l,m in e(J) if m!="0"]
     except:''
     print("+".join(o if o else "0"))
+"""

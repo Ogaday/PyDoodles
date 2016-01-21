981b89302347eb2513caba42b0519671dc4a932b
Ogaday <Ogaday@hotmail.co.uk>
2016-01-21 01:05:04 +0000

    Save two bytes with some smarter conditionals

--- notes.py
+++ notes.py
@@ -4,6 +4,8 @@ z='C@D@EF@G@A@B'
 def f(n):a,*b,c=n;return 2**((int(c)*12+z.index(a)-((9,7)[b==['#']],8)[not b]-49)/12)*440
 #2**((-48)/12)*440
 def f(n):a,*b,c=n;return 2**(int(c)+('C@D@EF@G@A@B'.find(a)-((22,20)[b==['#']],21)[not b])/12)*55    # 97 bytes
+#                                                                    '#'in b       b>[]
+def f(n):a,*b,c=n;return 2**(int(c)+('C@D@EF@G@A@B'.find(a)-(21,(22,20)['#'in b])[b>[]])/12)*55
 #f=lambda n:2**(int(n[-1])+('C@D@EF@G@A@B'.find(n[0])-((22,20)[n[1:-1]=='#'],21)[not n[1:-1]])/12)*55
 
 def convert(n):a,*b,c=n;return int(c)*12+z.index(a)-((9,7)[b==['#']],8)[not b]
